2793df7825d063ac980fd7b016d8aa334a52e402
taeheelee90 <taehee.lee90@gmail.com>
2020-09-02 21:19:17 +0300

    Changing dependencies

--- src/main/java/com/example/Bookstore/model/Book.java
+++ src/main/java/com/example/Bookstore/model/Book.java
@@ -6,20 +6,7 @@ public class Book {
 	private String author;
 	private int year;
 	private String isbn;
-	private double price;
-
-	public Book() {
-		super();
-	}
-
-	public Book(String title, String author, int year, String isbn, double price) {
-		super();
-		this.title = title;
-		this.author = author;
-		this.year = year;
-		this.isbn = isbn;
-		this.price = price;
-	}
+	private double price;	
 
 	public String getTitle() {
 		return title;
--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,28 +1,33 @@
 package com.example.Bookstore.web;
 
-//import java.util.ArrayList;
-//import java.util.List;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 import com.example.Bookstore.model.Book;
+
 
 @Controller
 public class BookController {
 
-	/*
-	 * @GetMapping("/books") public String bookController(Model model) { List<Book>
-	 * books = new ArrayList<Book>(); Book book1 = new Book("Java Cookbook",
-	 * "Darwin, Ian F.", 2001, "0-596-00170-3", 59.00 ); books.add(book1);
-	 * model.addAttribute("book", books); return "book"; }
-	 */
-
-	@GetMapping("/")
-	public String bookController(Book book) {
-		return "Title: " + book.getTitle() + "\nAuthor: " + book.getAuthor() + "\nYear: " + book.getYear() + "\nISBN: "
-				+ book.getIsbn() + "\nPrice: " + book.getPrice();
+	
+	 @GetMapping("/index")
+	 public String bookForm (Model model) { 
+		 model.addAttribute("book", new Book());
+		 return "bookForm";
 	}
+	
+	 @PostMapping ("/index")
+	 public String bookSubmit(@ModelAttribute Book book, Model model) {
+		 model.addAttribute("book", book);
+		 return "result";
+	 }
+	 
+	
+	 
 
 }
